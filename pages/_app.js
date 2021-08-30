8481a0b5c5d070916d034d02c3d1b718d21ca1c9
Rokas <rookas.rudzenskas@gmail.com>
2021-08-30 13:10:25 +0300

    Rokas

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,30 @@ import {CSSReset, ThemeProvider} from "@chakra-ui/core";
 import customTheme from "@/styles/theme";
 import {Head} from "next/document";
 
+const GlobalStyle = ({ children }) => {
+    return (
+        <>
+            <Head>
+                <meta content="width=device-width, initial-scale=1" name="viewport" />
+            </Head>
+            <CSSReset />
+                <Global
+                    styles={css`
+              html {
+                scroll-behavior: smooth;
+                min-width: 360px;
+              }
+              #__next {
+                display: flex;
+                flex-direction: column;
+                min-height: 100vh;
+              }
+            `}
+                />
+            {children}
+        </>
+    );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
