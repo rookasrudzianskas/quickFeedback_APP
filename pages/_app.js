1bb2037898b0d2d9d4364bab6a8302f2b35b6ebf
Rokas <rookas.rudzenskas@gmail.com>
2021-08-30 11:01:55 +0300

    Rokas

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
-import {ProvideAuth} from "../lib/auth";
+import {AuthProvider} from "../lib/auth";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <ProvideAuth>
+      <AuthProvider>
          <Component {...pageProps} />
-      </ProvideAuth>
+      </AuthProvider>
   )
 }
 
