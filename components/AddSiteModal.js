a37ae7243d5ccbce5c2daaf682b8a7ab33e8b7f2
Rokas <rookas.rudzenskas@gmail.com>
2021-08-31 14:51:08 +0300

    Rokas

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -14,130 +14,59 @@ import {useAuth} from "@/lib/auth";
 
 
 const InitialFocus = () => {
-    const auth = useAuth();
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { handleSubmit, register } = useForm();
     const initialRef = React.useRef();
 
+    const user = useAuth();
+    // console.log(user.user.uid);
     const onCreateSite = (values) => {
         createSite(values);
     }
 
     return (
         <>
-            {/*<Button*/}
-            {/*    id="add-site-modal-button"*/}
-            {/*    onClick={onOpen}*/}
-            {/*    backgroundColor="gray.900"*/}
-            {/*    color="white"*/}
-            {/*    fontWeight="medium"*/}
-            {/*    _hover={{ bg: 'gray.700' }}*/}
-            {/*    _active={{*/}
-            {/*        bg: 'gray.800',*/}
-            {/*        transform: 'scale(0.95)'*/}
-            {/*    }}*/}
-            {/*>*/}
-            {/*    /!*{children}*!/*/}
-            {/*</Button>*/}
             <Button onClick={onOpen} fontWeight="medium" maxW="200px" variant="solid" size="md">
                 Add Your First Site
             </Button>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
+            <Modal
+                initialFocusRef={initialRef}
+                isOpen={isOpen}
+                onClose={onClose}
+            >
                 <ModalOverlay />
-                <ModalContent as="form" onSubmit={handleSubmit(onCreateSite)}>
+                <ModalContent as="form"  onSubmit={handleSubmit(createSite)}>
                     <ModalHeader fontWeight="bold">Add Site</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody pb={6}>
                         <FormControl>
                             <FormLabel>Name</FormLabel>
                             <Input
-                                id="site-input"
+                                ref={initialRef}
                                 placeholder="My site"
-                                name="name"
-                                ref={register({
-                                    required: 'Required'
-                                })}
+                                name="site"
+                                {...register("site")}
                             />
                         </FormControl>
 
                         <FormControl mt={4}>
                             <FormLabel>Link</FormLabel>
                             <Input
-                                id="link-input"
-                                placeholder="https://website.com"
+                                placeholder="https://byroookas.com"
                                 name="url"
-                                ref={register({
-                                    required: 'Required'
-                                })}
+                                {...register("url")}
                             />
                         </FormControl>
                     </ModalBody>
 
                     <ModalFooter>
-                        <Button onClick={onClose} mr={3} fontWeight="medium">
-                            Cancel
-                        </Button>
-                        <Button
-                            id="create-site-button"
-                            backgroundColor="#99FFFE"
-                            color="#194D4C"
-                            fontWeight="medium"
-                            type="submit"
-                        >
-                            Create
-                        </Button>
+                        <Button onClick={onClose} mr={3} fontWeight="medium">Cancel</Button>
+                        <Button backgroundColor="#99FFEE" color="#194D4C" fontWeight="medium" type="submit" onClick={onCreateSite} >Create</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
-
-    //
-    // <>
-    //     <Button onClick={onOpen} fontWeight="medium" maxW="200px" variant="solid" size="md">
-    //         Add Your First Site
-    //     </Button>
-    //
-    //     <Modal
-    //         isOpen={isOpen} onClose={onClose}
-    //     >
-    //         <ModalOverlay />
-    //         <ModalContent as="form" onSubmit={handleSubmit(onCreateSite)}>
-    //             <ModalHeader fontWeight="bold">Add Site</ModalHeader>
-    //             <ModalCloseButton />
-    //             <ModalBody pb={6}>
-    //                 <FormControl>
-    //                     <FormLabel>Name</FormLabel>
-    //                     <Input
-    //                         id="site-input"
-    //                         placeholder="My site"
-    //                         name="name"
-    //                         ref={register({
-    //                             required: 'Required'
-    //                         })}
-    //                     />
-    //                 </FormControl>
-    //
-    //                 <FormControl mt={4}>
-    //                     <FormLabel>Link</FormLabel>
-    //                     <Input
-    //                         id="link-input"
-    //                         placeholder="https://website.com"
-    //                         name="url"
-    //                         ref={register({
-    //                             required: 'Required'
-    //                         })}
-    //                     />
-    //                 </FormControl>
-    //             </ModalBody>
-    //
-    //             <ModalFooter>
-    //                 <Button onClick={onClose} mr={3} fontWeight="medium">Cancel</Button>
-    //                 <Button backgroundColor="#99FFEE" color="#194D4C" fontWeight="medium" type="submit" onClick={onCreateSite} >Create</Button>
-    //             </ModalFooter>
-    //         </ModalContent>
-    //     </Modal>
-    // </>
     )
 }
 
