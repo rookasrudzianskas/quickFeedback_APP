2ef345f613772b1846ea0c542bf1efef36196269
Rokas <rookas.rudzenskas@gmail.com>
2021-08-31 15:46:59 +0300

    Rokas

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import {db} from "@/lib/firebase";
+
 export default function handler(_, res) {
   const  sitesRef = db.collection("sites");
 }
