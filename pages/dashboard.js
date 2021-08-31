2ee7bc120dab5ff67d8b1df1b79639042c2b481b
Rokas <rookas.rudzenskas@gmail.com>
2021-08-31 19:54:40 +0300

    Rokas

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -11,7 +11,6 @@ import useSWR from "swr";
 // import {useAuth} from "@/lib/auth";
 
 const Dashboard = () => {
-    const fetcher = (url) => fetch(url).then((res) => res.json());
     const { data, error } = useSWR('/api/sites', fetcher);
 
     console.log("Data", data);
