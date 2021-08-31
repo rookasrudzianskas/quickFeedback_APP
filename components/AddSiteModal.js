f2392c5eb55dbf990bc9154cf3dfa1bac1050b81
Rokas <rookas.rudzenskas@gmail.com>
2021-08-31 14:45:31 +0300

    Roaks

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -19,8 +19,8 @@ const InitialFocus = () => {
     const { handleSubmit, register } = useForm();
     const initialRef = React.useRef();
 
-    const onCreateSite = (values, uuid) => {
-        createSite(values, uuid);
+    const onCreateSite = (values) => {
+        createSite(values);
     }
 
     return (
@@ -91,6 +91,53 @@ const InitialFocus = () => {
                 </ModalContent>
             </Modal>
         </>
+
+    //
+    // <>
+    //     <Button onClick={onOpen} fontWeight="medium" maxW="200px" variant="solid" size="md">
+    //         Add Your First Site
+    //     </Button>
+    //
+    //     <Modal
+    //         isOpen={isOpen} onClose={onClose}
+    //     >
+    //         <ModalOverlay />
+    //         <ModalContent as="form" onSubmit={handleSubmit(onCreateSite)}>
+    //             <ModalHeader fontWeight="bold">Add Site</ModalHeader>
+    //             <ModalCloseButton />
+    //             <ModalBody pb={6}>
+    //                 <FormControl>
+    //                     <FormLabel>Name</FormLabel>
+    //                     <Input
+    //                         id="site-input"
+    //                         placeholder="My site"
+    //                         name="name"
+    //                         ref={register({
+    //                             required: 'Required'
+    //                         })}
+    //                     />
+    //                 </FormControl>
+    //
+    //                 <FormControl mt={4}>
+    //                     <FormLabel>Link</FormLabel>
+    //                     <Input
+    //                         id="link-input"
+    //                         placeholder="https://website.com"
+    //                         name="url"
+    //                         ref={register({
+    //                             required: 'Required'
+    //                         })}
+    //                     />
+    //                 </FormControl>
+    //             </ModalBody>
+    //
+    //             <ModalFooter>
+    //                 <Button onClick={onClose} mr={3} fontWeight="medium">Cancel</Button>
+    //                 <Button backgroundColor="#99FFEE" color="#194D4C" fontWeight="medium" type="submit" onClick={onCreateSite} >Create</Button>
+    //             </ModalFooter>
+    //         </ModalContent>
+    //     </Modal>
+    // </>
     )
 }
 
