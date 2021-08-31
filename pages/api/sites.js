7671c295d3983770f4ca81c11b4eb6c7686162ce
Rokas <rookas.rudzenskas@gmail.com>
2021-08-31 15:58:04 +0300

    Rokas

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -5,17 +5,22 @@ import {db} from "@/lib/firebase";
 export default function handler(_, res) {
   const sitesRef = db.collection("sites");
 
-  const getDoc = sitesRef.get().then((doc) => {
-    if(!doc.exists) {
-        console.log('No such document!');
-      } else {
-        console.log('Document found, data is:', doc.data());
-      }
-  })
-      .catch((err) =>  {
-        console.log("Error getting document", err);
-      })
+  const getDoc = sitesRef
+      .get()
+      .then((doc) => {
 
+        if(!doc.exists) {
+            console.log('No such document!');
+          }
+
+          res.status(200).json({ name: "Rokas" });
+
+      })
+          .catch((err) =>  {
+            console.log("Error getting document", err);
+          })
+
+        res.status(200).json({ name: "Rokas" });
 
 };
 
