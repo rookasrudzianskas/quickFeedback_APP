c625dbd43ad77ae31e58037ef23af94b76eaa9fa
Rokas <rookas.rudzenskas@gmail.com>
2021-09-01 09:04:20 +0300

    Rokas

--- pages/api/feedback/[siteId].js
+++ pages/api/feedback/[siteId].js
@@ -5,7 +5,7 @@ import {getAllFeedback} from "@/lib/db-admin";
 
 export default async function handler(req, res) {
     const siteId = req.query.siteId;
-    const feedback = getAllFeedback(siteId);
+    const feedback = await getAllFeedback(siteId);
 
     res.status(200).json({ feedback });
 
