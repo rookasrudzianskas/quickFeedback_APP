ac4b85be0fd5413b5dafde2e66657e67d32197cf
Rokas <rookas.rudzenskas@gmail.com>
2021-09-01 10:56:06 +0300

    Rokas'

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -4,7 +4,11 @@ import {db} from "@/lib/firebase";
 import {getAllSites} from "@/lib/db-admin";
 
 export default async function handler(_, res) {
-    const sites = await  getAllSites();
+    const { sites, error } = await  getAllSites();
+
+    if(error) {
+        res.status(500).json({ error: error });
+    }
 
     res.status(200).json({sites});
 
