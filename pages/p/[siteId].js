a5460cc6e4e3a3fe68a40e8289c11b03a6dd29c9
Rokas <rookas.rudzenskas@gmail.com>
2021-09-01 09:13:39 +0300

    Rokas

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,20 +1,3 @@
-import useSWR from "swr";
-import fetcher from "../../utils/fetcher";
-import {useAuth} from "@/lib/auth";
-import DashboardShell from "@/components/DashboardShell";
-import SiteTableSkeleton from "@/components/SiteTableSkeleton";
-import Head from "next/head";
-import SiteTable from "@/components/SiteTable";
-import EmptyState from "@/components/EmptyState";
-
-const SiteFeedback = () => {
-    return (
-        'Hello world'
-    )
-}
-
-export default SiteFeedback;
-
 export async function getStaticProps(context) {
     return {
         props: {
@@ -27,10 +10,20 @@ export async function getStaticPaths() {
     return {
         paths : [
             { params: {
-                siteId: 'CHpFwbzUUuNhCZHHbxX1'
+                    siteId: 'CHpFwbzUUuNhCZHHbxX1'
+                }
             }
-          }
         ],
         fallback: false
     };
 }
+
+const SiteFeedback = () => {
+    return (
+        'Hello world'
+    )
+}
+
+export default SiteFeedback;
+
+
