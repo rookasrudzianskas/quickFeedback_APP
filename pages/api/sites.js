17f9b0c94e86e6e2ccb67f53b041ece6b1efbba8
Rokas <rookas.rudzenskas@gmail.com>
2021-09-03 20:07:42 +0300

    Rokas

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -6,8 +6,8 @@ import {auth} from "@/lib/firebase-admin";
 export default async function handler(req, res) {
     // console.log(req.headers);
     // const token = req.headers.token;
-    const user = await auth.verifyIdToken(req.headers.token);
-    console.log("This is user something", user);
+    const { uid } = await auth.verifyIdToken(req.headers.token);
+    // console.log("This is user something", user);
     const { sites, error } = await  getAllSites();
 
     if(error) {
