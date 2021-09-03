e47f728cb9d1e0f46bbdde944d2043d04a738980
Rokas <rookas.rudzenskas@gmail.com>
2021-09-03 19:11:15 +0300

    Rokas

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,12 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import {getAllSites} from "@/lib/db-admin";
+import {auth} from "@/lib/firebase-admin";
 
 export default async function handler(req, res) {
     // console.log(req.headers);
     // const token = req.headers.token;
-    // const user = await auth.verifyIdToken(req.headers.token);
-    // console.log("This is user something", user);
+    const user = await auth.verifyIdToken(req.headers.token);
+    console.log("This is user something", user);
     const { sites, error } = await  getAllSites();
 
     if(error) {
