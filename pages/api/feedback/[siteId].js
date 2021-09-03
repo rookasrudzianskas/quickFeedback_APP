2e3ef371a9d3e2d1dacdac38db91134556e38cba
Rokas <rookas.rudzenskas@gmail.com>
2021-09-03 19:05:47 +0300

    Rokas

--- pages/api/feedback/[siteId].js
+++ pages/api/feedback/[siteId].js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import {db} from "@/lib/firebase-admin";
+import {db} from "@/lib/firebase";
 import {getAllFeedback} from "@/lib/db-admin";
 
 export default async function handler(req, res) {
