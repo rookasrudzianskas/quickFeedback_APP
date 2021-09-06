91f9cae51665be18ecb8a822a811d417671b3f62
Rokas <rookas.rudzenskas@gmail.com>
2021-09-06 08:48:17 +0300

    Rokas

---
+++ components/FeedbackTable.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Box, Link, Skeleton} from '@chakra-ui/core';
+import { Table, Tr, Th, Td } from './Table';
+import {format, parseISO} from "date-fns";
+import NextLink from 'next/link'
+
+
+const FeedbackTable = ({ sites }) => {
+    return (
+        <Table>
+            <thead>
+            <Tr>
+                <Th>Name</Th>
+                <Th>Site Link</Th>
+                <Th>Feedback Link</Th>
+                <Th>Date Added</Th>
+                <Th>{''}</Th>
+            </Tr>
+            </thead>
+            <tbody>
+            {sites.map(site => (
+
+                <Box key={site.id} as="tr">
+                    <Td fontWeight="medium">
+                        {site.name}
+                    </Td>
+                    <Td>
+                        {site.url}
+                    </Td>
+                    <Td>
+                        <NextLink href="/p/[siteId]" as={`/p/${site.id}`} passHref>
+                            <Link color="teal.500">View Feedback</Link>
+                        </NextLink>
+                    </Td>
+                    <Td>
+                        {format(parseISO(site.createdAt), 'PPpp')}
+                    </Td>
+                </Box>
+            ))}
+            </tbody>
+        </Table>
+    );
+};
+
+export default FeedbackTable;
