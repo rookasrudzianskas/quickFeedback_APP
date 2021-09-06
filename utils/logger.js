8226a31449169cadccdb7435e941211fdf69156e
Rokas <rookas.rudzenskas@gmail.com>
2021-09-06 12:48:28 +0300

    Rokas

--- utils/logger.js
+++ utils/logger.js
@@ -20,4 +20,19 @@ const logger = pino({
     }, stream
 );
 
+
+const formatObjectKeys = headers => {
+
+    const keyValues = {}
+
+    Object.keys(headers).map(key => {
+        const newKey = key.replace(/-/g, "_");
+        keyValues[newKey] = headers[key]
+    });
+
+    return keyValues
+}
+
+export { logger, formatObjectKeys }
+
 export default logger;
