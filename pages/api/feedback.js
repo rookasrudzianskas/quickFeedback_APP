ece127e1a8d249def98200769463005dfa7b372c
Rokas <rookas.rudzenskas@gmail.com>
2021-09-06 09:03:37 +0300

    Rokas

---
+++ pages/api/feedback.js
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import {getAllSites, getUserSites} from "@/lib/db-admin";
+import {auth} from "@/lib/firebase-admin";
+
+export default async function handler(req, res) {
+    // console.log(req.headers);
+    // const token = req.headers.token;
+    try {
+        const { uid } = await auth.verifyIdToken(req.headers.token);
+        // console.log("This is user something", user);
+        const {feedback} = await  getUserFeedback(uid);
+
+        res.status(200).json({feedback});
+
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+};
+
