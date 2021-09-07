81c8d01f812e96f9e30ce448bd0faefb2a2bbef2
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 19:28:21 +0300

    Rokas

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import {CSSReset, ThemeProvider} from "@chakra-ui/core";
 import customTheme from "@/styles/theme";
 import {Head} from "next/document";
 import { Global, css } from '@emotion/core';
+import {useEffect} from "react";
+import {DefaultSeo} from "next-seo";
+import SEO from "../next-seo.config";
 
 
 const GlobalStyle = ({ children }) => {
@@ -29,9 +32,20 @@ const GlobalStyle = ({ children }) => {
 };
 
 function MyApp({ Component, pageProps }) {
+
+    // useEffect(() => {
+    //     if (process.env.NODE_ENV === 'production') {
+    //         Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
+    //             includedDomains: ['fastfeedback.io']
+    //         });
+    //     }
+    // }, []);
+
   return (
        <ThemeProvider theme={customTheme}>
            <AuthProvider>
+               <DefaultSeo {...SEO} />
+
                <GlobalStyle />
                 <Component {...pageProps} />
            </AuthProvider>
