1654bfad13d1c71e3429d7fe2d7c88be0cc65d8a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 19:20:18 +0300

    Rokas

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+const title =
+    'Quick Feedback – The easiest way to add comments or reviews to your static site.';
+const description = 'Quick Feedback is being built as part of React 2025.';
+
+const SEO = {
+    title,
+    description,
+    canonical: 'https://www.quickfeedback.digital',
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://www.quickfeedback.digital',
+        title,
+        description,
+        images: [
+            {
+                url: 'https://fastfeedback.io/og.png',
+                alt: title,
+                width: 1280,
+                height: 720
+            }
+        ]
+    }
+};
+
+export default SEO;
