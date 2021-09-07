01ab04221c05f5dd239accac307ad457e9d5f3b3
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 17:30:20 +0300

    Rokas

--- components/UpgradeEmptyState.js
+++ components/UpgradeEmptyState.js
@@ -9,15 +9,36 @@ import {
 
 import DashboardShell from "@/components/DashboardShell";
 import AddSiteModal from "@/components/AddSiteModal";
+import {createCheckoutSession} from "@/lib/db";
+import {useAuth} from "@/lib/auth";
 
-const UpgradeEmptyState = () => (
+const UpgradeEmptyState = () => {
+
+    const user = useAuth();
+    return (
     <Flex width="100%" backgroundColor="white" borderRadius="8px" p={16} justify="center" direction="column" align="center">
-        <Heading mb={2} size="lg">You haven't added any sites.</Heading>
-        <Text mb={4}>Welcome, 👋. Let's get started.</Text>
-        <AddSiteModal>
-            Add Your First Site
-        </AddSiteModal>
+        <Heading mb={2} size="lg">Get feedback on your site instantly.</Heading>
+        <Text mb={4}>Start today, then grow with us 🌱</Text>
+
+        <Button
+            as="a"
+            onClick={(e) => createCheckoutSession(user.uid)}
+            // href="/dashboard"
+            backgroundColor="gray.900"
+            color="white"
+            fontWeight="medium"
+            mt={2}
+            maxW="200px"
+            _hover={{ bg: 'gray.700' }}
+            _active={{
+                bg: 'gray.800',
+                transform: 'scale(0.95)'
+            }}
+        >
+            Upgrade to Starter
+        </Button>
     </Flex>
-)
+    )
+}
 
 export default UpgradeEmptyState;
