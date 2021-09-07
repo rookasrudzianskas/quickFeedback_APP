5acc0655ce93a9f2e187f8806b4b561c50ca55df
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 19:26:13 +0300

    Rokas

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                         content="/favicons/browserconfig.xml"
                         name="msapplication-config"
                     />
-                    <link rel="icon" href="/assets/LOGO.png" />
+                    <link rel="icon" href="/favicon.ico" />
                     {/*<link href="/favicons/site.webmanifest" rel="manifest" />*/}
                     <link
                         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
