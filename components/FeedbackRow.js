7d706ff970f9fbad0d265bf1cbdbcc35de38ef71
Rokas <rookas.rudzenskas@gmail.com>
2021-09-08 08:56:26 +0300

    Rokas

--- components/FeedbackRow.js
+++ components/FeedbackRow.js
@@ -3,19 +3,26 @@ import {Box, Code, IconButton, Link, Skeleton, Switch} from '@chakra-ui/core';
 import { Table, Tr, Th, Td } from './Table';
 import RemoveButton from "@/components/RemoveButton";
 import {updateFeedback} from "@/lib/db";
+import {auth} from "@/lib/firebase-admin";
+import {useAuth} from "@/lib/auth";
+import {mutate} from "swr";
 
 
 const FeedbackRow = ({ id, author, text, route, status }) => {
 
     const [checked, setChecked] = useState(status === 'active');
+    const auth = useAuth();
 
     // console.log("This is feedback");
 
     const toggleFeedback = (e) => {
         setChecked(!checked);
-        updateFeedback(id, {status: checked ? 'active' : 'pending'})
+        updateFeedback(id, {status: !checked ? 'active' : 'pending'});
+        mutate(
+            ['/api/feedback', auth.user.token],
+        );
         // console.log(checked, id)
-    }
+    };
 
     return (
         <Box key={id} as="tr">
